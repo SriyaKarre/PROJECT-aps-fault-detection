a97a1d5f6a206703107c6ce918a243cd3602a4c9
sriyakarre <sriya165karre@gmail.com>
2023-03-12 11:56:30 +0530

    Added a new file .gitignore

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ REQUIREMENT_FILE_NAME="requirements.txt"
 def get_requirements()->List[str]:
     with open(REQUIREMENT_FILE_NAME) as requirements_file:
         requirements_list = requirements_file.readlines()
+    requirements_list = [requirement_name.replace("\n", "") for requirement_name in requirements_list]
+    if HYPHEN_E_DOT in requirements_list:
+        requirements_list.remove(HYPHEN_E_DOT)
+    return requirements_list
 
 setup(
     name="sensor",
